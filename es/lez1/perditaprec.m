d439243169572a3912afd95cae7d5d5d44b8c0c6
Federico <45485339+0xDE4DC0DE@users.noreply.github.com>
2020-03-25 14:32:22 +0100

    Update perditaprec.m

--- es/lez1/perditaprec.m
+++ es/lez1/perditaprec.m
@@ -1,8 +1,6 @@
-% scrivete queste istruzioni nel file perditaprec.m
-% le righe che cominciano con "%" sono commenti e vengono ignorate
 for k=1:300
-a=k*(1/k);
-if(a ~= 1)
-k %senza punto e virgola: scrive il valore di k
-end
+  a=k*(1/k);
+  if(a ~= 1)
+    k %senza punto e virgola: scrive il valore di k
+  end
 end
