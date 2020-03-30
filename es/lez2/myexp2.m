0ddc427ca0585a7a1a48ff6379cb4ce0ddd93662
Federico <45485339+0xDE4DC0DE@users.noreply.github.com>
2020-03-30 11:46:32 +0200

    Add files via upload

--- es/lez2/myexp2.m
+++ es/lez2/myexp2.m
@@ -1,13 +1,19 @@
-function s = myexp(x,n)
+function a = myexp2(x,n)
 %implementazione dell'esponenziale con il polinomio di taylor 
 %  arrestato al termine n-esimo
-s=1;
+t=1;
+a=1;
 if x>=0
     for k=1:n
-        s=s+pow(x, k)/fattoriale(k);
+        t=t*x/k;
+        a=a+t;
     end
 else 
-    s=1/(myexp(-x, n));
+    for k=1:n
+        t=t*(-x)/k;
+        a=a+t;
+    end
+    a=1/a;
 end
 %adesso s vale exp
 end
