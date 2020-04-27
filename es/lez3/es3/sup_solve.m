21139ca01055253b0e1cf034b110fc0bc69ce838
Federico <45485339+0xfederama@users.noreply.github.com>
2020-04-27 20:23:52 +0200

    Add files via upload

--- es/lez3/es3/sup_solve.m
+++ es/lez3/es3/sup_solve.m
@@ -1,12 +1,12 @@
-function x=sup_solve(U, b)
+function x=sup_solve(A, b)
 % Risolve b=U*x con U triangolare superiore
 n=length(b);
 x=zeros(n,1);
-for i=1:n
+for i=n:-1:1
     somma=0; % accumulatore
-    for j=i:n
-        somma=somma+U(i,j)*x(j);
+    for j=i+1:n
+        somma=somma+A(i,j)*x(j);
     end
-    x(i)=(b(i)-somma)/U(i,i);
+    x(i)=(b(i)-somma)/A(i,i);
 end
 end
